8f9e669d8a3e4cd01915997262dc30d1e5e00d0f
stdj91 <stdj.1907@gmail.com>
2022-01-28 21:54:45 +0100

    Task 5

--- main.js
+++ main.js
@@ -78,3 +78,26 @@
 // function concat(...args) {
 // 	return [].concat(...args)
 // }
+
+//<------------------------------------------------------------------------------->
+// Create a function that takes two numbers as arguments (num, length) and returns an array of multiples of num until the array length reaches length.
+
+// Examples
+// arrayOfMultiples(7, 5) ➞ [7, 14, 21, 28, 35]
+
+// arrayOfMultiples(12, 10) ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+
+// arrayOfMultiples(17, 6) ➞ [17, 34, 51, 68, 85, 102]
+// Notes
+// Notice that num is also included in the returned array.
+
+// solution: 1
+
+// function arrayOfMultiples (num, length) {
+// 	var result = []
+//     for (let i = 1; i <= length; i++) {
+//         result.push(num*i)
+        
+//     }
+//     return result
+// }
