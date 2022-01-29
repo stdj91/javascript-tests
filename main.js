f67ae2413db3a29acb8c45aaf74692b2eeeec70a
stdj91 <stdj.1907@gmail.com>
2022-01-29 21:34:32 +0100

    task 10

--- main.js
+++ main.js
@@ -173,3 +173,22 @@
 // function plus(a) {
 //     return a + 1;
 // }
+//<------------------------------------------------------------------------------->
+
+// task 10
+// You will be given an array of drinks, with each drink being an object with two properties: name and price. Create a function that has the drinks array as an argument and return the drinks objects sorted by price in ascending order.
+
+// Assume that the following array of drink objects needs to be sorted:
+
+// drinks = [
+//   {name: "lemonade", price: 50},
+//   {name: "lime", price: 10}
+// ]
+// The output of the sorted drinks object will be:
+//sortDrinkByPrice(drinks) ➞ [{name: "lime", price: 10}, {name: "lemonade", price: 50}]
+// function sortDrinkByPrice(drinks){
+// 	const sorted = drinks.sort(function(a, b){
+//     return a.price - b.price;
+// 	});
+// 	return sorted;
+// }
