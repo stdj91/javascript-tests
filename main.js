4c82df8e6191c1ea67baa23c4d6c2169ac460e8a
stdj91 <stdj.1907@gmail.com>
2022-01-29 21:27:36 +0100

    task 7

--- main.js
+++ main.js
@@ -130,4 +130,17 @@
 
 // solution 1
 // const shiftToLeft = (x, y) => x * 2 ** y
+//<------------------------------------------------------------------------------->
+//task 7
+// Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
 
+// Examples
+// cubes(3) ➞ 27
+
+// cubes(5) ➞ 125
+
+// cubes(10) ➞ 1000
+//Solution
+// function cubes(a) {
+// 	return a ** 3
+// }
