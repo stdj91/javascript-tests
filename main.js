659dac3d641e6bd90916cc2ef788ab65a01ddf24
stdj91 <stdj.1907@gmail.com>
2022-01-30 15:52:35 +0100

    task11

--- main.js
+++ main.js
@@ -192,3 +192,15 @@
 // 	});
 // 	return sorted;
 // }
+//<------------------------------------------------------------------------------->
+
+//task 11
+// print numbers from 1 to 100, but here’s the catch, multiple of three should print “Fizz” and similarly print “Buzz” 
+//for multiples of 5 and lastly print “FizzBuzz” for multiples of three and five.
+// solution 1
+// for (var i = 1; i < 101; i++) {
+//     if (i % 15 == 0) console.log("FizzBuzz");
+//     else if (i % 3 == 0) console.log("Fizz");
+//     else if (i % 5 == 0) console.log("Buzz");
+//     else console.log(i);
+// }
