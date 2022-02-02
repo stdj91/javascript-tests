bfa03826f131ceeda5109144f0b3765fb7e74e9e
stdj91 <stdj.1907@gmail.com>
2022-02-02 21:59:48 +0100

    task 14

--- main.js
+++ main.js
@@ -249,6 +249,29 @@
 
 //<------------------------------------------------------------------------------->
 //task 14
+//Coding challenge: Create a function that receives an array of numbers 
+//and returns an array containing only the positive numbers
+// function getPositives(ar)
+// {
+//     var ar2 = [];
+
+//     for(var i = 0; i < ar.length; i++)
+//     {
+//         var el = ar[i];
+
+//         if (el >= 0)
+//         {
+//             ar2.push(el);
+//         }
+//     }
+
+//     return ar2;
+// }
+
+// var ar = [-5, 10, -3, 12, -9, 5, 90, 0, 1];
+// var ar2 = getPositives(ar);
+
+// println(ar2);
 
 //<------------------------------------------------------------------------------->
 //task 15
