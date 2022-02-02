e7a3c822406a904ba8b7f0a3a547f34eef78b28f
stdj91 <stdj.1907@gmail.com>
2022-02-02 22:07:01 +0100

    Task 16

--- main.js
+++ main.js
@@ -296,6 +296,33 @@
 // println("Max: ", max);
 //<------------------------------------------------------------------------------->
 //task 16
+//Coding challenge: Create a function that will return a Boolean specifying if a number is prime
 
+// function isPrime(n)
+// {
+//     if (n < 2)
+//         return false;
+
+//     if (n == 2)
+//         return true;
+
+//     var maxDiv = Math.sqrt(n);
+
+//     for(var i = 2; i <= maxDiv; i++)
+//     {
+//         if (n % i == 0)
+//         {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
+// println(2, " is prime? ", isPrime(2));
+// println(3, " is prime? ", isPrime(3));
+// println(4, " is prime? ", isPrime(4));
+// println(5, " is prime? ", isPrime(5));
+// println(9, " is prime? ", isPrime(9));
 //<------------------------------------------------------------------------------->
-//task 16
+//task 17
