86900c51e2a533f69e4f0abb10a3b93cc29658a6
stdj91 <stdj.1907@gmail.com>
2022-02-02 22:02:55 +0100

    task 15

--- main.js
+++ main.js
@@ -275,7 +275,25 @@
 
 //<------------------------------------------------------------------------------->
 //task 15
+//Coding challenge: Find the maximum number in an array of numbers
+// function findMax(ar)
+// {
+//     var max = ar[0];
 
+//     for(var i = 0; i < ar.length; i++)
+//     {
+//         if (ar[i] > max)
+//         {
+//             max = ar[i];
+//         }
+//     }
+
+//     return max;
+// }
+
+// var ar = [-5, 10, -3, 12, -9, 5, 90, 0, 1];
+// var max = findMax(ar);
+// println("Max: ", max);
 //<------------------------------------------------------------------------------->
 //task 16
 
