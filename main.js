963cdab4a2cb8a5b6e5628a91e0c49c23c0bc573
Tuskar Tuskarr <stdj.1907@gmail.com>
2022-02-09 22:35:41 +0100

    Task 23

--- main.js
+++ main.js
@@ -514,3 +514,34 @@
 // };
 // var clock = new my_Clock();
 //   clock.run();
+
+//<------------------------------------------------------------------------------->
+
+//task23
+
+//Write a JavaScript function to merge two arrays and removes all duplicates elements.
+// function merge_array(array1, array2) {
+//     var result_array = [];
+//     var arr = array1.concat(array2);
+//     var len = arr.length;
+//     var assoc = {};
+
+//     while(len--) {
+//         var item = arr[len];
+
+//         if(!assoc[item]) 
+//         { 
+//             result_array.unshift(item);
+//             assoc[item] = true;
+//         }
+//     }
+
+//     return result_array;
+// }
+
+
+// var array1 = [1, 2, 3];
+
+// var array2 = [2, 30, 1];
+
+// console.log(merge_array(array1, array2));
