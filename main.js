e0fc067d071605d318f4a76cd0e64f83fe48e48e
Tuskar Tuskarr <stdj.1907@gmail.com>
2022-02-11 23:33:21 +0100

    task 25

--- main.js
+++ main.js
@@ -585,3 +585,25 @@
 //   console.log(difference([1, 2, 3], [100, 2, 1, 10]));
 //   console.log(difference([1, 2, 3, 4, 5], [1, [2], [3, [[4]]],[5,6]]));
 //   console.log(difference([1, 2, 3], [100, 2, 1, 10]));
+//<------------------------------------------------------------------------------->
+
+//task25
+//Write a JavaScript function to find the difference of two arrays
+//solution
+
+// function differenceOf2Arrays (array1, array2) {
+//     var temp = [];
+//     array1 = array1.toString().split(',').map(Number);
+//     array2 = array2.toString().split(',').map(Number);
+    
+//     for (var i in array1) {
+//     if(array2.indexOf(array1[i]) === -1) temp.push(array1[i]);
+//     }
+//     for(i in array2) {
+//     if(array1.indexOf(array2[i]) === -1) temp.push(array2[i]);
+//     }
+//     return temp.sort((a,b) => a-b);
+//     }
+    
+//     console.log(differenceOf2Arrays([1, 2, 3], [100, 2, 1, 10]));
+//     console.log(differenceOf2Arrays([1, 2, 3, 4, 5], [1, [2], [3, [[4]]],[5,6]]));
