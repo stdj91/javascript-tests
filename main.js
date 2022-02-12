e15285b2e38842935c4e7bdf83b2a1686fbe13f6
Tuskar Tuskarr <stdj.1907@gmail.com>
2022-02-12 21:12:02 +0100

    task 26

--- main.js
+++ main.js
@@ -607,3 +607,31 @@
     
 //     console.log(differenceOf2Arrays([1, 2, 3], [100, 2, 1, 10]));
 //     console.log(differenceOf2Arrays([1, 2, 3, 4, 5], [1, [2], [3, [[4]]],[5,6]]));
+//<------------------------------------------------------------------------------->
+
+//task26
+// Write a JavaScript program to display the colors in the following way.
+
+// Here is the sample array:
+// color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+// o = ["th","st","nd","rd"]
+// Output
+// "1st choice is Blue ."
+// "2nd choice is Green."
+// "3rd choice is Red."
+var color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+
+function Ordinal(n)
+ {
+ var o = ["th","st","nd","rd"],
+ x = n%100;
+ return x+(o[(x-20)%10]||o[x]||o[0]);
+ }
+
+for(n = 0; n < color.length; n++){
+ var ordinal = n + 1;
+
+ var output = (Ordinal(ordinal) + " choice is " + color[n] + ".");
+
+console.log(output);
+}
